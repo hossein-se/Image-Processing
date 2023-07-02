3aee0b62a519ec35709b73b10e265fb8e4d037f4
hossein <amirseyedghasemi@gmail.com>
2023-07-02 10:41:03 +0330

    Apply filters and see results

--- filters.py
+++ filters.py
@@ -12,6 +12,26 @@ img2 = cv.resize(img2, (300, 300))
 gauss = cv.GaussianBlur(img2, (7,7), 0)
 
 
+#gmask
+gmask = cv.subtract(img2,gauss)
+
+#highboost filtering
+k=14
+img_unsharp = cv.addWeighted(img2, 1, gmask, k, 0)
+
+#show results
+cv.imshow('image_orginal',img2)
+cv.imshow('image_gauss',gauss)
+cv.imshow('image_mask',gmask)
+cv.imshow('imag_final',img_unsharp)
+
+#save results
+cv.imwrite('image_orginal.jpg', img2)
+cv.imwrite('image_gauss.jpg', gauss)
+cv.imwrite('image_mask.jpg', gmask)
+cv.imwrite('imag_final.jpg', img_unsharp)
+
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()
